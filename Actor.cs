794ab07f76d070c912ab1ae5032fa60b0c578700
andersS91 <anders-soerensen@hotmail.com>
2017-04-23 13:49:52 +0200

    First Commit

---
+++ Actor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Actor : MonoBehaviour {
+    private int travel = 0;
+    private NavMeshAgent agent;
+    private Vector3 destination;
+    private bool calculated = false;
+    private bool grown;
+
+    // Use this for initialization
+    void Awake () {
+        agent = GetComponent<NavMeshAgent>();
+        travel = -1;
+    }
+    
+    private void Update()
+    {
+        agent.SetDestination(destination);
+    }
+    
+    public int GetTravel()
+    {
+        return travel;
+    }
+
+    public void SetTravel(int travel)
+    {
+        this.travel = travel;
+    }
+
+    public bool GetCalculated()
+    {
+        return calculated;
+    }
+
+    public void SetCalculated(bool calculated) {
+        this.calculated = calculated;
+    }
+
+    public void SetDestination(int id, Vector3 destination)
+    {
+        SetTravel(id);
+        this.destination = destination;
+    }
+    
+    public Vector3 GetDestination()
+    {
+        return agent.destination;
+    }
+}
