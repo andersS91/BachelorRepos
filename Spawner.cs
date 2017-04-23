166ff7f7be2fb88c1e53ca277da0745897405cc5
andersS91 <anders-soerensen@hotmail.com>
2017-04-23 13:51:09 +0200

    Scripts1

---
+++ Spawner.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+using UnityEngine.AI;
+
+public class Spawner : MonoBehaviour {
+    //Public variables
+    public int projectedActivity;
+    public int popinThreshold;
+    public float maxRange;
+    public float minRange;
+    public bool torusOfLife;
+    public bool stressTest;
+    public bool destroyOnContact;
+    public GameObject[] actorPrefabs;
+    public GameObject FPC;
+
+    //Private variables
+    private int actorsInPlay = 0;
+    private int totalProb = 0;
+    private int lastSpawn = 0;
+    private int lastProb = 0;
+    private List<Waypoint> spawns = new List<Waypoint>();
+    private float temp;
+    private int newProb;
+    private int spawnProb;
+    private bool initSpawn = false;
+    private float bias = 0.4f;
+    private string FPCTag;
+
+    void Start()
+    {
+        if(FPC != null)
+        {
+            FPCTag = FPC.tag;
+        }
+        newProb = 0;
+        Time.timeScale = 2;
+        if (stressTest)
+        {
+            StressTest();
+        } else
+        {
+            Initialize();
+        }
+    }
+
+    private void Initialize()
+    {
+        StartCoroutine(InitSpawn(.5f));
+    }
+
+    void Update()
+    {
+        if(stressTest || (!destroyOnContact && initSpawn && projectedActivity > actorsInPlay))
+        {
+            Spawn();
+            return;
+        }
+    }
+
+    IEnumerator InitSpawn(float waitTime)
+    {
+        //bool temp = torusOfLife;
+        //torusOfLife = false;
+        if (waitTime > 0)
+        {
+            while(projectedActivity > actorsInPlay)
+            {
+                Spawn();
+                yield return new WaitForSeconds(waitTime);
+            }
+        }
+        //torusOfLife = temp;
+        if (projectedActivity <= actorsInPlay)
+        {
+            initSpawn = true;
+        }
+    }
+
+    public bool TorusOfLife(GameObject actor)
+    {
+        if(FPC != null && Vector3.Distance(actor.transform.position, FPC.transform.position) > maxRange)
+        {
+            if (FPCNotInView(actor.transform.position))
+            {
+                Despawn(actor);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void StressTest()
+    {
+        float spawnTime = 1.0f/bias;
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    private List<Waypoint> WaypointsInRange()
+    {
+        List<Waypoint> inRange = new List<Waypoint>();
+        Vector3 pos;
+        for (int i = 0; i < spawns.Count; i++)
+        {
+            pos = spawns[i].transform.position;
+            if (Vector3.Distance(pos, FPC.transform.position) > minRange && Vector3.Distance(pos, FPC.transform.position) < maxRange)
+            {
+                inRange.Add(spawns[i]);
+            }
+        }
+        return inRange;
+    }
+
+    private void SpawnNonTorus()
+    {
+        if (lastProb > 0)
+        {
+            spawnProb = lastProb;
+        }
+        else
+        {
+            spawnProb = 0;
+            for (int i = 0; i < spawns.Count; i++)
+            {
+                if (spawns[i].GetInstanceID() == lastSpawn)
+                {
+                    spawnProb = spawns[i].spawnProb;
+                    break;
+                }
+            }
+        }
+        newProb = totalProb - spawnProb;
+        temp = UnityEngine.Random.Range(0, totalProb);
+        for (int j = 0; j < spawns.Count; j++)
+        {
+            if (spawns[j].GetInstanceID() != lastSpawn)
+            {
+                temp -= spawns[j].spawnProb;
+                if (temp < 0)
+                {
+                    lastSpawn = spawns[j].GetInstanceID();
+                    lastProb = spawns[j].spawnProb;
+                    Instantiate(actorPrefabs[(j % actorPrefabs.Length)], spawns[j].transform.position, spawns[j].transform.rotation).GetComponent<NavMeshAgent>().speed = bias + UnityEngine.Random.Range(0, 0.5f * bias);
+                    actorsInPlay++;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void SpawnTorus(List<Waypoint> inRange)
+    {
+        if (inRange == null || inRange.Count <= 0)
+        {
+            return;
+        }
+        for (int i = 0; i < inRange.Count; i++)
+        {
+            newProb += inRange[i].spawnProb;
+        }
+        temp = UnityEngine.Random.Range(0, newProb);
+        for (int j = 0; j < inRange.Count; j++)
+        {
+            //if (waypoints[j].GetInstanceID() != lastSpawn)
+            //{
+                newProb -= inRange[j].spawnProb;
+                if (newProb < 0 && FPCNotInView(inRange[j].transform.position))
+                {
+                    lastSpawn = inRange[j].GetInstanceID();
+                    lastProb = inRange[j].spawnProb;
+                    Instantiate(actorPrefabs[(j % actorPrefabs.Length)], inRange[j].transform.position, inRange[j].transform.rotation).GetComponent<NavMeshAgent>().speed = bias + UnityEngine.Random.Range(0, 0.5f * bias);
+                    actorsInPlay++;
+                    break;
+                }
+            //}
+        }
+
+    }
+
+    private bool FPCNotInView(Vector3 pos)
+    {
+        RaycastHit hit;
+        Vector3 dir = pos - FPC.transform.position;
+        for(int i = 0; i < popinThreshold; i++)
+        {
+            if(Physics.Raycast(pos, dir, out hit, dir.magnitude)){
+                if (hit.collider.tag == FPCTag)
+                {
+                    return false;
+                }
+            }
+            Wait(0.1f);
+        }
+        return true;
+    }
+
+    private void Spawn()
+    {
+        if (!torusOfLife) {
+            SpawnNonTorus();
+        }
+        else
+        {
+            SpawnTorus(WaypointsInRange());
+        }
+    }
+
+    public void Despawn(GameObject actor)
+    {
+        Destroy(actor);
+        actorsInPlay--;
+    }
+
+    private void WaitingForInit(float waitTime)
+    {
+        if(waitTime > 0)
+        {
+            while(!initSpawn)
+            {
+                Wait(waitTime);
+            }
+        }
+    }
+
+    IEnumerable Wait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+    }
+
+    public void AddSpawn(Waypoint spawn)
+    {
+        spawns.Add(spawn);
+        totalProb += spawn.spawnProb;
+    }
+}
---
+++ Waypoint.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    //Connections
+    public Waypoint[] waypoints;
+    public float[] flux;
+    public bool spawn = false;
+    private float[] fill;
+    public int spawnProb;
+    public GameObject spawnObject;
+    private Spawner spawner;
+
+    //ChooseEdge
+    float total = 0;
+    int newDirId = 0;
+    float temp = 100000;
+    bool filled = true;
+
+    // Use this for initialization
+    void Awake()
+    {
+        spawner = spawnObject.GetComponent<Spawner>();
+        if (spawn)
+        {
+            spawner.AddSpawn(gameObject.GetComponent<Waypoint>());
+        }
+        fill = new float[waypoints.Length];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            fill[i] = 0;
+        }
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            if (flux[i] < 0)
+            {
+                flux[i] = 1 / (0 - flux[i]);
+            }
+        }
+    }
+
+    private void Start()
+    {
+    }
+
+    public int EnterEdge(int id)
+    {
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            if (waypoints[i].GetInstanceID() == id)
+            {
+                fill[i]++;
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public void DecEdge(int id)
+    {
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            if (waypoints[i].GetInstanceID() == id)
+            {
+                if (fill[i] > 0)
+                {
+                    fill[i]--;
+                }
+                break;
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other == null)
+        {
+            return;
+        }
+
+        if (other.GetComponent<Actor>() != null)
+        {
+            Actor actor = other.GetComponent<Actor>();
+            if (actor.GetTravel() == this.GetInstanceID())
+            {
+                return;
+            }
+            int id = actor.GetTravel();
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                if (id == waypoints[i].GetInstanceID())
+                {
+                    if (spawn)
+                    {
+                        spawner.Despawn(other.gameObject);
+                    }
+                    else
+                    {
+                       if(spawner.TorusOfLife(other.gameObject))
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+            RouteCal(other, actor);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other == null)
+        {
+            return;
+        }
+        return;
+    }
+
+    private void RouteCal(Collider other, Actor actor)
+    {
+        if (other != null)
+        {
+            int id = actor.GetTravel();
+            LeaveEdge(id);
+            actor.SetDestination(this.GetInstanceID(), waypoints[EnterEdge(ChooseEdge(id))].transform.position);
+        }
+        return;
+    }
+
+    private int ChooseEdge(int dirId)
+    {
+        total = 0;
+        float[] P = new float[waypoints.Length];
+        newDirId = 0;
+        temp = 100000;
+        filled = true;
+
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            if (dirId != waypoints[i].GetInstanceID())
+            {
+                if (flux[i] - fill[i] > 0)
+                {
+                    P[i] = flux[i] - fill[i];
+                    total += P[i];
+                    filled = false;
+                }
+            }
+            else
+            {
+                P[i] = 0;
+            }
+        }
+
+
+        if (filled)
+        {
+            int chosen = -1;
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                if (flux[i] > 0 && dirId != waypoints[i].GetInstanceID() && (((float)fill[i]) / flux[i] < temp))
+                {
+                    chosen = i;
+                    newDirId = waypoints[i].GetInstanceID();
+                    temp = fill[i] / flux[i];
+                }
+            }
+            if (chosen > -1)
+            {
+                fill[chosen]++;
+            }
+
+        }
+        else
+        {
+            temp = Random.Range(0, total);
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                temp -= P[i];
+                if (temp < 0)
+                {
+                    fill[i]++;
+                    newDirId = waypoints[i].GetInstanceID();
+                    break;
+                }
+            }
+        }
+
+        return newDirId;
+    }
+
+    private void LeaveEdge(int id)
+    {
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            if (waypoints[i].GetInstanceID() == id)
+            {
+                waypoints[i].DecEdge(this.GetInstanceID());
+                break;
+            }
+        }
+    }
+
+
+
+    public int GetId()
+    {
+        return this.GetInstanceID();
+    }
+
+}
